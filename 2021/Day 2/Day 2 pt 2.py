7bfec1c43f80cd74ac25eebebf4bc1f8d38c4664
Griffin Milas <me@griffinmilas.com>
2021-12-02 02:00:13 -0500

    Day 2 2021

--- 2021/Day 2/Day 2 pt 2.py
+++ 2021/Day 2/Day 2 pt 2.py
@@ -2,6 +2,7 @@ import sys
 
 sys.setrecursionlimit(2500)
 
+
 # Just finished, but realized this could prob be a dict instead of a list... oopsie
 # pos is [hor, depth, aim]
 
@@ -21,7 +22,7 @@ def compute_position(instruction, pos=None):
 
 
 def forward(magnitude, pos):
-    return [pos[0] + magnitude, pos[1] + magnitude*pos[2], pos[2]]
+    return [pos[0] + magnitude, pos[1] + magnitude * pos[2], pos[2]]
 
 
 def up(magnitude, pos):
@@ -39,4 +40,4 @@ cur_pos = compute_position(lines[0])
 for i in range(1, len(lines)):
     cur_pos = compute_position(lines[i], cur_pos)
 
-print("FINAL ANSWER DAY 1 PT 1: ", cur_pos[0]*cur_pos[1])
+print("FINAL ANSWER DAY 1 PT 1: ", cur_pos[0] * cur_pos[1])
